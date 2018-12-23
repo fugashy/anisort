88729bff23b6e2a553c3cf37dbbb6323444fe800
fugashy <fugashy@icloud.com>
2018-12-23 18:16:31 +0900

    Add select sort

--- src/sort.py
+++ src/sort.py
@@ -5,6 +5,9 @@ def create(config_dict):
     if config_dict['type'] == 'bubble':
         lower = config_dict['lower']
         return BubbleSort(lower)
+    elif config_dict['type'] == 'select':
+        lower = config_dict['lower']
+        return SelectSort(lower)
     else:
         raise NotImplementedError(
                 '{} is not implemented'.format(config_dict['type']))
@@ -20,7 +23,10 @@ class BubbleSort:
         Args:
             lower: 降順にする(bool)
         """
-        self.__lower = lower
+        if lower:
+            self.__check = lambda a, b: True if a <= b else False
+        else:
+            self.__check = lambda a, b: True if a > b else False
 
     def sort(self, data):
         u"""
@@ -33,16 +39,32 @@ class BubbleSort:
             True: 完了
             False: まだやれる
         """
-        if self.__lower:
-            check = lambda a, b: True if a <= b else False
-        else:
-            check = lambda a, b: True if a > b else False
 
         swapped = False
         for i in range(len(data) - 1):
-            if check(data[i], data[i + 1]):
+            if self.__check(data[i], data[i + 1]):
                 data[i], data[i + 1] = data[i + 1], data[i]
                 swapped = True
 
         return not swapped
 
+
+class SelectSort:
+    def __init__(self, lower):
+        self.__index = 0
+        if lower:
+            self.__get_id = lambda data: max(
+                    range(self.__index, len(data)), key=data.__getitem__)
+        else:
+            self.__get_id = lambda data: min(
+                    range(self.__index, len(data)), key=data.__getitem__)
+
+
+
+    def sort(self, data):
+        target_index = self.__get_id(data)
+        data[self.__index], data[target_index] = data[target_index], data[self.__index]
+        self.__index += 1
+        if self.__index == len(data):
+            return True
+        return False
